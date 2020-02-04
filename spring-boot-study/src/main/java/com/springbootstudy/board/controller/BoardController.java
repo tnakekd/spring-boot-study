cf39a87f40c877610f8674e76436391ef82d40d6
song <tnakekd@gmail.com>
2020-02-04 23:27:55 +0900

    logback 추가

--- spring-boot-study/src/main/java/com/springbootstudy/board/controller/BoardController.java
+++ spring-boot-study/src/main/java/com/springbootstudy/board/controller/BoardController.java
@@ -2,6 +2,8 @@ package com.springbootstudy.board.controller;
 
 import com.springbootstudy.board.service.BoardService;
 import com.springbootstudy.board.vo.BoardVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,13 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/board/openBoardList")
     public ModelAndView openBoardList() throws Exception {
         ModelAndView mv = new ModelAndView("/board/boardList");
+
+        logger.debug("openBoardList");
 
         List<BoardVO> list = boardService.selectBoardList();
         mv.addObject("list", list);
